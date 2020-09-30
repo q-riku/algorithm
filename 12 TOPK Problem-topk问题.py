055bb8440e833e6d5887b036c943faf9759f92de
q-riku <q-riku@users.noreply.github.com>
2020-09-30 22:25:12 +0900

    algorithm

--- 12 TOPK Problem-topk问题.py
+++ 12 TOPK Problem-topk问题.py
@@ -41,7 +41,7 @@ def topk(li, k):
     for i in range((k - 2) // 2, -1, -1):
         sift(heap, i, k - 1)
     # 2.遍历
-    for i in range(k, len(li) - 1):
+    for i in range(k, len(li)):
         if li[i] > heap[0]:
             heap[0] = li[i]
             sift(heap, 0, k - 1)
@@ -53,7 +53,7 @@ def topk(li, k):
 
 
 import random
-li = list(range(1000))
+li = list(range(10))
 random.shuffle(li)
 
-print(topk(li, 10))
+print(topk(li, 5))
