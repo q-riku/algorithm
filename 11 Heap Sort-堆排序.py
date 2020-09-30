69ca2a5a9fc856c1694acb6ce09a64d85d481c5a
q-riku <q-riku@users.noreply.github.com>
2020-09-30 20:38:56 +0900

    algorithm

--- 11 Heap Sort-堆排序.py
+++ 11 Heap Sort-堆排序.py
@@ -66,6 +66,7 @@
     4 6 8
 ·堆的向下调整性质
     当根节点的左右 子树都是堆但自身不是堆时，可以通过一次向下的调整来将其变换成一个堆
+·时间复杂度：O(nlogn)
 -------------------------------------------------------------------------------
 11.2.2 堆排序过程
 ·1.建立堆
@@ -73,6 +74,17 @@
 ·3.去掉堆顶，将堆最后一个元素放到堆顶，此时可通过一次调整重新使堆有序
 ·4堆顶元素为第二大元素
 ·5.重复步骤3，直到堆变空
+-------------------------------------------------------------------------------
+11.2.3 内置模块
+·Python内置模块--heapq
+    import heapq
+·常用函数
+    heapify(x)
+    heappush(heap,item)
+    heappop(heap)
+
+===============================================================================
+===============================================================================
 """
 
 
@@ -84,10 +96,10 @@ def sift(li, low, high):
     :param high:堆的最后一个元素的位置
     """
     i = low  # i最开始指向根节点
-    j = 2*i + 1  # j是左子节点
+    j = 2 * i + 1  # j是左子节点
     tmp = li[low]  # 把堆顶存起来
-    while j <= high: # 只要j位置有数
-        if j + 1 <= high and li[j+1] > li[j]:  # 如果右子节点比较大
+    while j <= high:  # 只要j位置有数
+        if j + 1 <= high and li[j + 1] > li[j]:  # 如果右子节点比较大
             j = j + 1  # j指向右子节点
         if li[j] > tmp:
             li[i] = li[j]
@@ -99,3 +111,32 @@ def sift(li, low, high):
     else:
         li[i] = tmp  # 把tmp放到叶子节点上
 
+
+def heap_sort(li):
+    n = len(li)
+    for i in range((n - 2) // 2, -1, -1):  # i表示建堆的时候调整的部分的根的下标
+        sift(li, i, n - 1)  # 建堆完成
+    for i in range(n - 1, -1, -1):  # i指向当前堆的最后一个元素
+        li[0], li[i] = li[i], li[0]
+        sift(li, 0, i - 1)  # i-1是新的high
+
+
+li = [6, 8, 1, 9, 3, 0, 7, 2, 4, 5]
+heap_sort(li)
+print(li)
+
+
+# ========== heapq ==========
+import heapq  # q-->queue优先队列
+import random
+
+li2 = list(range(100))
+random.shuffle(li2)
+
+print(li2)
+
+heapq.heapify(li2)  # 建堆，默认建小根堆
+# print(li2)
+
+for i in range(len(li2)):
+    print(heapq.heappop(li2), end=',')  # 每次弹出最小的值
